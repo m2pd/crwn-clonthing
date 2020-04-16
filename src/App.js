8c52be3245e76f630ff1d5d728dc1dd161e670ac
Minh Phượng Đoàn <doanminhphuong.208@tcu.edu.vn>
2020-04-16 21:52:50 +0700

    header

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import React from 'react';
 import { Switch, Route } from 'react-router-dom';
 import HomePage from './pages/HomePage';
 import ShopPage from './pages/ShopPage';
+import Header from './components/Header';
 import './App.css';
 
 
 function App() {
   return (
       <div className="App">
+        <Header />
         <Switch>
           <Route exact path='/' component = {HomePage} />
           <Route path='/shop' component = {ShopPage} />
