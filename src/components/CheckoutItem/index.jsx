2543dc79ec27e4830526b364c06c2b36bcf913ed
Minh Phượng Đoàn <doanminhphuong.208@tcu.edu.vn>
2020-06-25 11:29:41 +0700

    add and remove item checkout-item  page

--- src/components/CheckoutItem/index.jsx
+++ src/components/CheckoutItem/index.jsx
@@ -1,18 +1,33 @@
 import React from 'react'
 
+import { connect } from 'react-redux'
+import { clearItemFromCart, addItem, removeItem } from '../../redux/cart/cart.action'
+
 import './checkout-item.style.scss'
 
-const CheckoutItem = ({cartItem: {name, imageUrl, price, quantity}}) =>(
-    <div className="checkout-item">
-        <div className="image-container">
-            <img src={imageUrl} alt="item"/>
+const CheckoutItem = ({cartItem,clearItem, addItem, removeItem}) =>{
+    const {name, imageUrl, price, quantity} = cartItem;
+    return(
+        <div className="checkout-item">
+            <div className="image-container">
+                <img src={imageUrl} alt="item"/>
+            </div>
+            <div className="name">{name}</div>
+            <div className="quantity">
+                <span className="arrow" onClick={() => removeItem(cartItem)}>&#10094;</span>
+                    <span className="value">{quantity}</span>
+                <span className="arrow" onClick={() => addItem(cartItem)}>&#10095;</span>
+                </div>
+            <div className="price">{price}</div>
+            <div className="remove-button" onClick={() => clearItem(cartItem)}>&#10005;</div>
         </div>
-        <div className="name">{name}</div>
-        <div className="quantity">{quantity}</div>
-        <div className="price">{price}</div>
-        <div className="remove-button">&#10005;</div>
-    </div>
-)
+)}
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch =>({
+    clearItem : item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
 
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -13,3 +13,20 @@ export const addItemToCart = (cartItems,cartItemAdd) =>{
 
     return [...cartItems, {...cartItemAdd, quantity: 1 }]
 };
+
+export const removeItemToCart = (cartItems, cartItemRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemRemove.id
+    )
+
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter(cartItem => cartItem.id !== cartItemRemove.id)
+    }
+
+    return cartItems.map(cartItem =>
+        cartItem.id === cartItemRemove.id
+        ? {...cartItem, quantity : cartItem.quantity - 1}
+        : cartItem
+    );
+
+}
